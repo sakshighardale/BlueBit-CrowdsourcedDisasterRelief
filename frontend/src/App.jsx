c4839570f5d0aad442b906da66f338bdb5b9da85
Nish077 <nishant6073@gmail.com>
2025-03-22 23:12:10 +0530

    Initialized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-blue-500'>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
